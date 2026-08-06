d703c3c5e9d039c8c17acf684a544346ff19ce54
Marcin Bielak <marcin.bieli@gmail.com>
2026-08-06 22:58:39 +0200

    Add PolicyStatus and JobType enums with CanEqual

--- modules/policy/src/main/scala/com/github/bieli/openinsuranceengine/policy/Policy.scala
+++ modules/policy/src/main/scala/com/github/bieli/openinsuranceengine/policy/Policy.scala
@@ -2,3 +2,12 @@ package com.github.bieli.openinsuranceengine.policy
 
 enum PolicyStatus:
   case Quote, Draft, Quoted, Bound, InForce, Cancelled, Expired, NonRenewed, Reinstated
+
+object PolicyStatus:
+  given CanEqual[PolicyStatus, PolicyStatus] = CanEqual.derived
+
+enum JobType:
+  case Submission, PolicyChange, Renewal, Cancellation, Reinstatement, Rewrite, Audit
+
+object JobType:
+  given CanEqual[JobType, JobType] = CanEqual.derived
