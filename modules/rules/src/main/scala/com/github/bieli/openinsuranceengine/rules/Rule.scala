a387dd8550043246f085ac5b9633853441b0c0d7
Marcin Bielak <marcin.bieli@gmail.com>
2026-08-06 23:14:56 +0200

    Add Rule trait and RuleAction enum for rules engine

---
+++ modules/rules/src/main/scala/com/github/bieli/openinsuranceengine/rules/Rule.scala
@@ -0,0 +1,24 @@
+package com.github.bieli.openinsuranceengine.rules
+
+import cats.data.NonEmptyList
+import com.github.bieli.openinsuranceengine.core.result.{DomainError, DomainResult}
+
+/**
+ * Generic business-rules engine inspired by domain rules / Rule Sets.
+ */
+trait Rule[C]:
+  def id: String
+  def name: String
+  def priority: Int
+  def enabled: Boolean
+  def evaluate(ctx: C): DomainResult[RuleOutcome[C]]
+
+enum RuleAction:
+  case Accept
+  case Reject(reason: String)
+  case Modify
+  case Refer(reason: String) // underwriting referral
+  case Warn(message: String)
+
+object RuleAction:
+  given CanEqual[RuleAction, RuleAction] = CanEqual.derived
