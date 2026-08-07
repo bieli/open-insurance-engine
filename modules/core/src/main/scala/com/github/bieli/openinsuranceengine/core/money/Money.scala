2d54ad23d448b4696c5e25c34a379ee9e9adf335
Marcin Bielak <marcin.bieli@gmail.com>
2026-08-07 21:08:47 +0200

    Update Money.scala

--- modules/core/src/main/scala/com/github/bieli/openinsuranceengine/core/money/Money.scala
+++ modules/core/src/main/scala/com/github/bieli/openinsuranceengine/core/money/Money.scala
@@ -1,7 +1,5 @@
 package com.github.bieli.openinsuranceengine.core.money
 
-import cats.Monoid
-
 /** ISO-4217 currency code as opaque type. */
 opaque type CurrencyCode = String
 
@@ -18,3 +16,27 @@ object CurrencyCode:
 
   extension (c: CurrencyCode) def value: String = c
   given CanEqual[CurrencyCode, CurrencyCode] = CanEqual.derived
+
+/**
+ * Money represented in minor units (grosze / cents) to avoid floating-point
+ * arithmetic - standard practice in financial / insurance cores.
+ */
+final case class Money(amountMinor: Long, currency: CurrencyCode):
+  def +(other: Money): Either[String, Money] =
+    if currency == other.currency then Right(Money(amountMinor + other.amountMinor, currency))
+    else Left(s"Currency mismatch: ${currency.value} vs ${other.currency.value}")
+
+  def -(other: Money): Either[String, Money] =
+    if currency == other.currency then Right(Money(amountMinor - other.amountMinor, currency))
+    else Left(s"Currency mismatch: ${currency.value} vs ${other.currency.value}")
+
+  def *(factor: BigDecimal): Money =
+    Money((BigDecimal(amountMinor) * factor).setScale(0, BigDecimal.RoundingMode.HALF_UP).toLong, currency)
+
+  def isPositive: Boolean = amountMinor > 0
+  def isZero: Boolean = amountMinor == 0
+  def negate: Money = Money(-amountMinor, currency)
+
+  def toMajor: BigDecimal = BigDecimal(amountMinor) / 100
+
+  override def toString: String = f"${toMajor}%1.2f ${currency.value}"
