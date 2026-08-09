2dda96c280f96f67f2fd607f3e65920fa13cf1ee
Marcin Bielak <marcin.bieli@gmail.com>
2026-08-09 11:44:18 +0200

    Add VehicleRisk and PropertyRisk case classes

--- modules/core/src/main/scala/com/github/bieli/openinsuranceengine/core/risk/Risk.scala
+++ modules/core/src/main/scala/com/github/bieli/openinsuranceengine/core/risk/Risk.scala
@@ -10,3 +10,33 @@ sealed trait RiskUnit:
   def riskType: String
   def description: String
   def attributes: Map[String, String]
+
+final case class VehicleRisk(
+    vin: String,
+    make: String,
+    model: String,
+    year: Int,
+    licensePlate: Option[String] = None,
+    annualMileage: Option[Int] = None,
+    attributes: Map[String, String] = Map.empty
+) extends RiskUnit:
+  val riskType: String = "vehicle"
+  def description: String = s"$year $make $model ($vin)"
+
+final case class PropertyRisk(
+    addressLine: String,
+    city: String,
+    postalCode: String,
+    constructionType: Option[String] = None,
+    yearBuilt: Option[Int] = None,
+    replacementCost: Option[Money] = None,
+    attributes: Map[String, String] = Map.empty
+) extends RiskUnit:
+  val riskType: String = "property"
+  def description: String = s"$addressLine, $city $postalCode"
+
+final case class GenericRisk(
+    riskType: String,
+    description: String,
+    attributes: Map[String, String] = Map.empty
+) extends RiskUnit
